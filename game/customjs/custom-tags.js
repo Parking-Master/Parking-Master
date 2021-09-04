8473c33cb3daa6ea3195ce91b740f4e270c71de8
Parking Master <parkingmaster@email.com>
2021-09-04 17:30:54 -0400

    Update custom-tags.js

--- game/customjs/custom-tags.js
+++ game/customjs/custom-tags.js
@@ -1,4 +1,9 @@
 document.createElement('url');
+var customT_styles = document.createElement('link');
+customT_styles.rel = 'stylesheet';
+customT_styles.type = 'text/css';
+customT_styles.href = 'https://cdn.jsdelivr.net/gh/Parking-Master/Parking-Master/game/customjs/custom-tags.css';
+document.head.appendChild(customT_styles);
 check_text$$ = setTimeout(function(){
 document.documentElement.innerHTML = document.documentElement.innerHTML.replace('<url' + document.querySelector('url').attributes + '>', '<url' + document.querySelector('url').attributes + '></url;');
 document.querySelector('url').innerText = document.querySelector('url').getAttribute('text');
