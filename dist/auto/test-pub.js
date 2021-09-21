1ced48b4fd02ed9a8e017f10feb7d9ca57230b08
Parking Master <parkingmaster@email.com>
2021-09-21 18:43:02 -0400

    Update test-pub.js

--- dist/auto/test-pub.js
+++ dist/auto/test-pub.js
@@ -4,47 +4,43 @@
     subscribeKey: "sub-c-e609faec-1b29-11ec-9ca7-5693d1c31269",
     uuid: uuid
   });
-
-  const button = document.getElementById('publish-button');
-
-  button.addEventListener('click', () => {
+  function message(c){
     pubnub.publish({
       channel : "pubnub_onboarding_channel",
-      message : {"sender": uuid, "content": "Hello From JavaScript SDK"}
+      message : {"sender": uuid, "content": c}
     }, function(status, response) {
-      //Handle error here
+      document.write('<pre>'+response+'</pre>');
     });
-  });
+  }
 
   pubnub.subscribe({
     channels: ['pubnub_onboarding_channel'],
     withPresence: true
   });
-
+  function tag(t){
+  return document.querySelector(t);
+  }
+  function cs(c){
+  return document.querySelector('.'+c);
+  }
+  function id(i){
+  return document.getElementById(i);
+  }
   pubnub.addListener({
-    message: function(event) {
-      let pElement = document.createElement('p');
-      pElement.appendChild(document.createTextNode(event.message.content));
-      document.body.appendChild(pElement);
-    },
-    presence: function(event) {
-      let pElement = document.createElement('p');
-      pElement.appendChild(document.createTextNode(event.uuid + " has joined. That's you!"));
-      document.body.appendChild(pElement);
+    message: function(e,y) {
+      !y||y==null||y=='undefined'||message.arguments.length===1?id('message').textContent+=e.message.content:tag(y).textContent+=e.message.content;
     }
   });
 
-  pubnub.history(
-    {
+  pubnub.history({
       channel: 'pubnub_onboarding_channel',
       count: 10,
       stringifiedTimeToken: true,
     },
-    function (status, response) {
+    function(status, response) {
       let pElement = document.createElement('h3');
       pElement.appendChild(document.createTextNode('historical messages'));
       document.body.appendChild(pElement);
-
       pElement = document.createElement('ul');
       let msgs = response.messages;
       for (let i in msgs) {
